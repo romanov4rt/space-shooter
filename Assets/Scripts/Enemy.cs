a24969979c8663a0525d781cffa781ad80877286
Artem <Romanov.4rt@gmail.com>
2022-12-06 22:40:14 +0400

    added enemy spawn manager, damage system +asset pack

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -20,4 +20,24 @@ public class Enemy : MonoBehaviour
             transform.position = new Vector3(Random.Range(-8f, 8f), 8, 0);
         }
     }
+
+    void OnTriggerEnter(Collider other) 
+    {
+        if(other.tag == "Player")
+        {
+            Player player = other.transform.GetComponent<Player>();
+
+            if(player != null)
+            {
+                player.Damage();
+            }
+
+            Destroy(this.gameObject);
+        }
+        if(other.tag == "Laser")
+        {
+            Destroy(other.gameObject);
+            Destroy(this.gameObject);
+        }
+    }
 }
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField] private GameObject _enemyPrefab;
+    [SerializeField] private GameObject _enemyContainer;
+
+    private bool _stopSpawning = false;
+    
+    void Start()
+    {
+        StartCoroutine(SpawnRoutine());
+    }
+
+    IEnumerator SpawnRoutine()
+    {
+        while (_stopSpawning == false)
+        {
+            Vector3 posToSpawn = new Vector3(Random.Range(-8, 8), 8, 0);
+            GameObject newEnemy = Instantiate(_enemyPrefab, posToSpawn, Quaternion.identity);
+            newEnemy.transform.parent = _enemyContainer.transform;
+            yield return new WaitForSeconds(2.0f);
+        }
+    }
+
+    public void OnPlayerDeath()
+    {
+        _stopSpawning = true;
+    }
+}
